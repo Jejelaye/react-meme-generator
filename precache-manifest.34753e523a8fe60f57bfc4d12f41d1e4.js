460687479350e59479a02547e33f014424cd8d7f
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2019-03-04 22:00:49 +0100

    Updates

---
+++ precache-manifest.34753e523a8fe60f57bfc4d12f41d1e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8e5c09ca0d700400085",
+    "url": "jejelaye.github.io/react-meme-generator/static/css/main.7d9f60bf.chunk.css"
+  },
+  {
+    "revision": "b8e5c09ca0d700400085",
+    "url": "jejelaye.github.io/react-meme-generator/static/js/main.b8e5c09c.chunk.js"
+  },
+  {
+    "revision": "c10ef927986e1cb241c6",
+    "url": "jejelaye.github.io/react-meme-generator/static/css/1.92b31e17.chunk.css"
+  },
+  {
+    "revision": "c10ef927986e1cb241c6",
+    "url": "jejelaye.github.io/react-meme-generator/static/js/1.c10ef927.chunk.js"
+  },
+  {
+    "revision": "34742f676ccc902e2263",
+    "url": "jejelaye.github.io/react-meme-generator/static/js/runtime~main.34742f67.js"
+  },
+  {
+    "revision": "bb7471f26f4ce04bd293bc1c002622a1",
+    "url": "jejelaye.github.io/react-meme-generator/index.html"
+  }
+];
